0f31e731a2fbd786c44de702e22df17991eefb39
User1 <sevenmuratt@gmail.com>
2023-06-02 20:34:37 +0300

    RandomQuote2

--- script.js
+++ script.js
@@ -2,35 +2,47 @@ const btnEl = document.getElementById("btn");
 const quoteEl = document.getElementById("quote");
 const authorEl = document.getElementById("author");
 
-const apiUrl = "https://api.quotable.io/random";
+const apiURL = "https://api.quotable.io/random";
 
-/* function getQuote() {
-    fetch(apiUrl)
-        .then((res) => res.json())
-        .then((data) => {
-            quoteEl.innerText = data.content;
-            authorEl.innerText = data.author;
-        });
-} */
+async function getQuote() {
+  try {
+    btnEl.innerText = "Loading...";
+    btnEl.disabled = true;
+    quoteEl.innerText = "Updating...";
+    authorEl.innerText = "Updating...";
 
-async function getQuote(){
-    const repsonse = await fetch(apiUrl);
-    const data = await repsonse.json();
-    
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
     const quoteContent = data.content;
     const author = data.author;
-
     const length = data.length;
-    if(length>100){
-        quoteEl.style.fontSize = "7px";
+
+    if (length > 200) {
+      quoteEl.style.fontSize = "1.5vw";
+      authorEl.style.fontSize = "1.2vw";
+    } else if (length > 130) {
+      quoteEl.style.fontSize = "2.4vw";
+      authorEl.style.fontSize = "2.2vw";
+    } else {
+      quoteEl.style.fontSize = "3vw";
+      authorEl.style.fontSize = "2.6vw";
     }
+
     quoteEl.innerText = quoteContent;
     authorEl.innerText = author;
 
-
-    //console.log(data);
-    
-}   
+    btnEl.innerText = "Get a quote";
+    btnEl.disabled = false;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    quoteEl.innerText = "An error happened, try again later";
+    authorEl.innerText = "An error happened";
+    btnEl.innerText = "Get a quote";
+    btnEl.disabled = false;
+  }
+}
 
 getQuote();
 btnEl.addEventListener("click", getQuote);
